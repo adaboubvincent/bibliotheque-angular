7836425714559ac3666233e7882a96b906a7d83e
adaboubvincent <adaboubvincent@gmail.com>
2021-04-03 19:42:44 +0000

    updates to deploy to heroku

--- src/app/services/generique/DAO.ts
+++ src/app/services/generique/DAO.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
     providedIn: 'root'
